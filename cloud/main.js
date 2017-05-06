9c181fb03194430b4fbda82967784822a3d2334e
Kevin Thrailkill <kevinjt92@me.com>
2017-05-05 17:46:05 -0700

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -5,12 +5,12 @@ Parse.Cloud.define('hello', function(req, res) {
 
 
 Parse.Cloud.afterDelete("Trip", function(request) {
- Parse.Cloud.useMasterKey;
  query = new Parse.Query("Destination");
  query.equalTo("trip", request.object);
- query.find({
+ query.find({ useMasterKey: true,
    success: function(destinations) {
      Parse.Object.destroyAll(destinations, {
+       useMasterKey: true,
        success: function() {},
        error: function(error) {
          console.error("Error deleting related destinations " + error.code + ": " + error.message);
@@ -25,12 +25,12 @@ Parse.Cloud.afterDelete("Trip", function(request) {
 
 
 Parse.Cloud.afterDelete("Destination", function(request) {
- Parse.Cloud.useMasterKey;
  query = new Parse.Query("Plan");
  query.equalTo("destination", request.object);
- query.find({
+ query.find({ useMasterKey: true,
    success: function(plans) {
      Parse.Object.destroyAll(plans, {
+       useMasterKey: true,
        success: function() {},
        error: function(error) {
          console.error("Error deleting related plans " + error.code + ": " + error.message);
