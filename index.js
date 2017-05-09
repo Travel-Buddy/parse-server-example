dbb76de70a3321eba0d8a081b6d6171167fe135b
Kevin Thrailkill <kevinjt92@me.com>
2017-05-08 23:51:54 -0700

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: '/home/myApp/cloud/main.js',
-  verbose: true,
  // cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
